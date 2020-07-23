99e646ae2714df4404b2549be115e5530c8d68cc
Etienne Marais <hello@etiennemarais.co.za>
2020-07-23 13:35:06 +0200

    :robot: Start up package :rocket:

--- example/index.js
+++ example/index.js
@@ -1,47 +1,15 @@
-# nodemailer-mjml-mustache
-A plugin for nodemailer that uses mjml and mustache view engine to generate emails.
-
-## Installation
-
-```sh
-npm install nodemailer-mjml-mustache --save
-```
-
-### Plugin Options
-
-- `viewPath` **(required)** provides the path to the directory where your views are
-
-### Mail Options
-
-- `template` the name of the template file to use without the extension
-- `context` this will be passed to the view engine as the context data to render the variables with.
-
-```js
-// example
-context: {
-  name: `Bob Ross`,
-  url: `https://mjml.io/try-it-live`,
-}
-```
-
-```html
-<!-- MJML Example-->
-<mj-text>Hello {{name}}, <a href="{{url}}">Click here</a></mj-text>
-```
-
-## Example Usage
-
-```js
 let nodemailer = require(`nodemailer`)
 let mjml = require(`../lib`)
 let htmlToText = require(`nodemailer-html-to-text`).htmlToText
 
 // Setup local version of mailcatcher
+console.log("Setting up nodemailer")
 let transporter = nodemailer.createTransport({
   host: '127.0.0.1',
   port: 1025,
 })
 
+console.log("Setting up nodemailer plugins")
 // Compile the mjml template to html with parsing the mustache variables
 transporter.use(`compile`, mjml({
 	viewPath: `${__dirname}/views`
@@ -67,9 +35,10 @@ console.log("Sending options", options)
 new Promise((resolve, reject) => {
   transporter.sendMail(options, function(error, info){
       if (error) {
+        console.log("Email failed to send")
         return reject(error)
       }
+      console.log("Email sent")
       resolve(info)
   })
 })
-```
